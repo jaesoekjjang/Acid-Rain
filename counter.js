3161eff63693fce6be13b7d08d0373ebd7a3142d
jaesoek <ljs981120@gmail.com>
2023-01-16 18:22:56 +0900

    Add game-over

--- counter.js
+++ counter.js
@@ -15,111 +15,145 @@ import {
   timeout,
   startWith,
   take,
+  timer,
 } from "rxjs";
 
 import { ajax } from "rxjs/ajax";
 
-const INTERVAL = 2000;
-const TIME_LIMIT = 10000;
+// // export const setupCounter = (element) => {
+// //   const counter = new BehaviorSubject(0);
 
-export const setupCounter = (element) => {
-  const counter = new BehaviorSubject(0);
+// //   const setInnerHTML = (value) => (element.innerHTML = `count is ${value}`);
 
-  const setInnerHTML = (value) => (element.innerHTML = `count is ${value}`);
+// //   fromEvent(element, "click")
+// //     .pipe(tap(() => counter.next(counter.getValue() + 1)))
+// //     .subscribe(() => setInnerHTML(counter.getValue()));
 
-  fromEvent(element, "click")
-    .pipe(tap(() => counter.next(counter.getValue() + 1)))
-    .subscribe(() => setInnerHTML(counter.getValue()));
+// //   setInnerHTML(counter.getValue());
+// // };
 
-  setInnerHTML(counter.getValue());
+// // export const decreasingInterval = () => {
+// //   const interval$ = new BehaviorSubject(1);
+
+// //   return interval$.pipe(
+// //     switchMap(() => interval(interval$.getValue() * TIME_LIMIT)),
+// //     tap((_) => interval$.next(interval$.getValue() * 0.999))
+// //   );
+// // };
+
+// // const words = new BehaviorSubject([]);
+
+// // decreasingInterval()
+// //   .pipe(
+// //     mergeMap(() => ajax("https://randomuser.me/api")),
+// //     map((x) => x?.response?.results[0]?.name?.first),
+// //     timestamp(),
+// //     tap((x) => console.log(Date.now(), x)),
+
+// //     // tap(_ => words.next([...words.getValue(), x]))
+// //     scan((acc, x) => [...acc, x], [])
+// //   )
+// //   .subscribe((w) => {
+// //     console.log(w);
+// // const $names = document.querySelector(".names");
+
+// // const $li = document.createElement("li");
+// // const $div = document.createElement("div");
+// // $div.innerText = w;
+
+// // $li.appendChild($div);
+// // $names.appendChild($li);
+// // });
+
+// // export const inputListener = (element) => {
+// //   fromEvent(element, "keyup")
+// //     .pipe(
+// //       map((x) => x.code),
+// //       filter((x) => {
+// //         return Date.now() - x.timestamp < TIME_LIMIT;
+// //       })
+// //     )
+// //     .subscribe();
+// // };
+
+const difficultyMap = {
+  1: [3000, 10000],
+  2: [2000, 7000],
+  3: [1500, 5000],
+  4: [1500, 3000],
 };
 
-export const decreasingInterval = () => {
-  const interval$ = new BehaviorSubject(1);
+export function init(difficulty = 2) {
+  const [INTERVAL, TIME_LIMIT] = difficultyMap[difficulty];
 
-  return interval$.pipe(
-    switchMap(() => interval(interval$.getValue() * TIME_LIMIT)),
-    tap((_) => interval$.next(interval$.getValue() * 0.999))
+  const subscriptions = [];
+
+  const words$ = new BehaviorSubject({});
+  const life$ = new BehaviorSubject(1);
+
+  const word$ = interval(INTERVAL).pipe(
+    mergeMap(() => ajax("https://randomuser.me/api")),
+    map((x) => x?.response?.results[0]?.name?.first),
+    tap((x) => {
+      words$.next({ ...words$.getValue(), [x]: x });
+      timer(TIME_LIMIT).subscribe(() => {
+        const words = words$.getValue();
+        if (x in words) {
+          life$.next(life$.getValue() - 1);
+          const wordsCopy = { ...words };
+          delete wordsCopy[x];
+          words$.next(wordsCopy);
+        }
+      });
+    })
   );
-};
 
-const words = new BehaviorSubject([]);
+  const input$ = fromEvent(document.querySelector("input"), "keyup").pipe(
+    map((x) => x?.target?.value),
+    tap((x) => {
+      const words = words$.getValue();
+      if (x in words) {
+        const copy = { ...words };
+        delete copy[x];
+        words$.next(copy);
+        document.querySelector("input").value = "";
+      }
+    })
+  );
 
-// decreasingInterval()
-//   .pipe(
-//     mergeMap(() => ajax("https://randomuser.me/api")),
-//     map((x) => x?.response?.results[0]?.name?.first),
-//     timestamp(),
-//     tap((x) => console.log(Date.now(), x)),
-
-//     // tap(_ => words.next([...words.getValue(), x]))
-//     scan((acc, x) => [...acc, x], [])
-//   )
-//   .subscribe((w) => {
-//     console.log(w);
-// const $names = document.querySelector(".names");
-
-// const $li = document.createElement("li");
-// const $div = document.createElement("div");
-// $div.innerText = w;
-
-// $li.appendChild($div);
-// $names.appendChild($li);
-// });
-
-// export const inputListener = (element) => {
-//   fromEvent(element, "keyup")
-//     .pipe(
-//       map((x) => x.code),
-//       filter((x) => {
-//         return Date.now() - x.timestamp < TIME_LIMIT;
-//       })
-//     )
-//     .subscribe();
-// };
-
-const words$ = new BehaviorSubject({});
-
-const click$ = fromEvent(document, "click");
-
-const interval$ = interval(INTERVAL);
-const word$ = interval$.pipe(
-  mergeMap(() => ajax("https://randomuser.me/api")),
-  map((x) => x?.response?.results[0]?.name?.first),
-  timestamp(),
-  tap((x) => words$.next({ ...words$.getValue(), [x.value]: x })),
-  tap((x) => {
-    const words = words$.getValue();
-    const timeoverWords = Object.values(words).filter(
-      ({ timestamp }) => Date.now() - timestamp > TIME_LIMIT
+  const wordSubscription = word$.subscribe();
+  const inputSubscription = input$.subscribe();
+  const wordsSubscription = words$.subscribe((words) => {
+    document.querySelector(".words-list").innerHTML = Object.keys(words).reduce(
+      (acc, word) => acc + `<li>${word}</li>`,
+      ""
     );
-    if (timeoverWords.length > 0) {
-      const copy = { ...words };
-      timeoverWords.forEach(({ value }) => delete copy[value]);
-      words$.next(copy);
+  });
+  const lifeSubscription = life$.subscribe((life) => {
+    document.querySelector(".life").innerHTML = life;
+    if (life <= 0) {
+      gameOver();
+      return;
     }
-  })
-);
+  });
 
-word$.subscribe();
-const input$ = fromEvent(document.querySelector("input"), "keyup").pipe(
-  map((x) => x?.target?.value),
-  tap((x) => {
-    const words = words$.getValue();
-    if (x in words) {
-      const copy = { ...words };
-      delete copy[x];
-      words$.next(copy);
-      document.querySelector("input").value = "";
-    }
-  })
-);
-
-input$.subscribe();
-
-words$.subscribe((words) => {
-  document.querySelector("ul").innerHTML = Object.keys(words).reduce(
-    (acc, word) => acc + `<li>${word}</li>`,
-    ""
+  subscriptions.push(
+    wordSubscription,
+    inputSubscription,
+    wordsSubscription,
+    lifeSubscription
   );
-});
+
+  function unSubscribe() {
+    subscriptions.forEach((s) => s.unsubscribe());
+  }
+
+  function gameOver() {
+    unSubscribe();
+    showGameOver();
+  }
+
+  function showGameOver() {
+    document.querySelector(".game-over__panel").classList.remove("play");
+  }
+}
--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
+import { init } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
-  <div>
-    <ul class='names'>
-    </ul>
-    <input class='input'  type="text" />
+<div>
+  <div class='game__panel'>
+    <div>
+      <span class="life">123</span>
+    </div>
+    <ul class='words-list'></ul>
+    <input class="input" type="text" />
   </div>
+  <div class='game-over__panel play'>
+    <div class='game-over__text'>
+      <span>Game Over</span>
+    </div>
+    <div>
+      <button class='game-over__button'>
+      <span>Play Again</span>
+      </button>
+    </div>
+  </div>
+</div>
 `;
 
-// setupCounter(document.querySelector("#counter"));
-// decreasingInterval();
-// inputListener(document.querySelector("input"));
+init();
