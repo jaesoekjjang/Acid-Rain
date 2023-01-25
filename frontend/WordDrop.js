ea691fa418fcde8fc0830fd096dde9aab1f8df43
jaesoek <ljs981120@gmail.com>
2023-01-25 18:01:48 +0900

    Chore add backend workspace

---
+++ frontend/WordDrop.js
@@ -0,0 +1,43 @@
+export function WordDrop(text) {
+  this.text = text;
+  this.speed = WordDrop.speed(WordDrop.count++);
+  this.x = WordDrop.x(text.length);
+  this.y = 0;
+}
+
+WordDrop.count = 0;
+
+WordDrop.init = function ({ canvasWidth, minSpeed, maxSpeed, fontSize }) {
+  WordDrop.canvasWidth = canvasWidth;
+  WordDrop.minSpeed = minSpeed;
+  WordDrop.maxSpeed = maxSpeed;
+  WordDrop.fontSize = fontSize;
+};
+
+WordDrop.speed = function (n) {
+  const { minSpeed, maxSpeed } = WordDrop;
+  const speed = Math.random() + minSpeed * 1.02 ** n;
+  return speed > maxSpeed ? maxSpeed : speed;
+};
+
+WordDrop.x = function (textLength) {
+  const { canvasWidth, fontSize } = WordDrop;
+  return Math.random() * (canvasWidth - textLength * fontSize);
+};
+
+WordDrop.prototype.draw = function (ctx) {
+  ctx.fillText(this.text, this.x, this.y);
+};
+
+WordDrop.prototype.update = function (canvas, words$, life$) {
+  this.y += this.speed;
+
+  if (this.y <= canvas.height) return;
+
+  const words = words$.getValue();
+  const copy = { ...words };
+  delete copy[this.text];
+  words$.next(copy);
+
+  life$.next(life$.getValue() - 1);
+};
