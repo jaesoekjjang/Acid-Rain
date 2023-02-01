894b3a44b9ee74de663063141372887cf730eb9e
jaesoek <ljs981120@gmail.com>
2023-02-02 02:49:23 +0900

    [frontend] Add Element

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,93 +1,128 @@
 import "../style.css";
 import { Modal } from "./component/Modal";
-import { Component } from "./Component";
+import { Component, Element } from "./Component";
 import { GamePanel } from "./component/GamePanel";
 import { Game } from "./models/Game";
 import { loadText } from "./utils";
 import { init } from "./router";
 import { Ranking } from "./component/Ranking";
+import { fromEvent } from "rxjs";
 
 const wordList = (await loadText(import.meta.env.VITE_WORDS_PATH)).split(/\s+/);
 
+class Moddal extends Component {
+  template({ isPlaying }) {
+    return isPlaying ? new Element("div", null, ["modal"]) : null;
+  }
+
+  onMount() {}
+}
+
 class App extends Component {
+  template(state) {
+    return new Element("div", null, [
+      new Element("button", { class: "start" }, ["start"]),
+      new Element("button", { class: "stop" }, ["stop"]),
+      new Element("span", null, [new Moddal(state)]),
+    ]);
+  }
+
   onMount() {
-    const container = document.querySelector(".container");
-
-    const gamePanel = new GamePanel(
-      container,
-      {
-        tag: "div",
-        attrs: { class: "game_panel" },
-      },
-      {
-        setIsPlaying: (value) => this.setState("isPlaying", value),
-      }
+    const setIsPlaying = (v) => this.setState("isPlaying", v);
+    fromEvent(document.querySelector(".start"), "click").subscribe(() =>
+      setIsPlaying(true)
     );
-
-    const $canvas = document.querySelector("#canvas");
-    const $fieldset = document.querySelector(".game-fieldset");
-    const $form = document.querySelector(".game-form");
-    const $input = $form.querySelector("input");
-    const $life = document.querySelector(".life");
-    const game = new Game({ $canvas, $form, $life });
-
-    const modal = new Modal(
-      container,
-      { tag: "div" },
-      {
-        game,
-        isPlaying: () => this.getState("isPlaying"),
-        setIsPlaying: (value) => this.setState("isPlaying", value),
-        score: () => this.getState("score"),
-      }
+    fromEvent(document.querySelector(".stop"), "click").subscribe(() =>
+      setIsPlaying(false)
     );
-
-    this.addComponents([gamePanel, modal]);
-
-    const onGameStart = () => {
-      //! 여기서 시작하자마자 한번 리렌더링 됨
-      this.setState("isPlaying", true);
-      $fieldset.disabled = false;
-      $input.focus();
-    };
-
-    const onGameOver = () => {
-      this.setState("isPlaying", false);
-      this.setState("score", game.getScore());
-      $fieldset.disabled = true;
-    };
-
-    game.init({ wordList, onGameStart, onGameOver });
-
-    //TODO 난이도 설정 UI
-    game.setDifficulty(4);
-    game.start();
   }
 }
 
-const route = () => {
-  const { pathname } = location;
-  document.querySelector("#app").innerHTML = "";
+new App({ isPlaying: true }).render(document.querySelector("#app"));
 
-  if (pathname === "/") {
-    const canvas = document.createElement("CANVAS");
-    canvas.setAttribute("id", "canvas");
-    document.querySelector("#app").appendChild(canvas);
-    new App(
-      document.querySelector("#app"),
-      { tag: "div", attrs: { class: "container" } },
-      { isPlaying: true, score: 0 }
-    );
-  }
+// class App extends Component {
+//   template() {
 
-  if (pathname === "/ranking") {
-    new Ranking(document.querySelector("#app"), {
-      tag: "div",
-      attrs: { class: "ranking" },
-    });
-  }
-};
+//   }
 
-init(route);
-route();
-addEventListener("popstate", route);
+//   onMount() {
+//     const container = document.querySelector(".container");
+
+//     const gamePanel = new GamePanel(
+//       container,
+//       {
+//         tag: "div",
+//         attrs: { class: "game_panel" },
+//       },
+//       {
+//         setIsPlaying: (value) => this.setState("isPlaying", value),
+//       }
+//     );
+
+//     const $canvas = document.querySelector("#canvas");
+//     const $fieldset = document.querySelector(".game-fieldset");
+//     const $form = document.querySelector(".game-form");
+//     const $input = $form.querySelector("input");
+//     const $life = document.querySelector(".life");
+//     const game = new Game({ $canvas, $form, $life });
+
+//     const modal = new Modal(
+//       container,
+//       { tag: "div" },
+//       {
+//         game,
+//         isPlaying: () => this.getState("isPlaying"),
+//         setIsPlaying: (value) => this.setState("isPlaying", value),
+//         score: () => this.getState("score"),
+//       }
+//     );
+
+//     this.addComponents([gamePanel, modal]);
+
+//     const onGameStart = () => {
+//       //! 여기서 시작하자마자 한번 리렌더링 됨
+//       this.setState("isPlaying", true);
+//       $fieldset.disabled = false;
+//       $input.focus();
+//     };
+
+//     const onGameOver = () => {
+//       this.setState("isPlaying", false);
+//       this.setState("score", game.getScore());
+//       $fieldset.disabled = true;
+//     };
+
+//     game.init({ wordList, onGameStart, onGameOver });
+
+//     //TODO 난이도 설정 UI
+//     game.setDifficulty(4);
+//     game.start();
+//   }
+// }
+
+// const route = () => {
+//   const { pathname } = location;
+//   document.querySelector("#app").innerHTML = "";
+
+//   if (pathname === "/") {
+//     const canvas = document.createElement("CANVAS");
+//     canvas.setAttribute("id", "canvas");
+//     document.querySelector("#app").appendChild(canvas);
+//     new App(
+//       document.querySelector("#app"),
+//       { tag: "div", attrs: { class: "container" } },
+//       { isPlaying: true, score: 0 }
+//     );
+//   }
+
+//   if (pathname === "/ranking") {
+//     new Ranking(document.querySelector("#app"), {
+//       tag: "div",
+//       attrs: { class: "ranking" },
+//     });
+//   }
+// };
+
+// init(route);
+// route();
+// addEventListener("popstate", route);
--- node_modules/frontend/src/Component.js
+++ node_modules/frontend/src/Component.js
@@ -1,7 +1,6 @@
 import { BehaviorSubject, filter, map, pairwise } from "rxjs";
 
 export class Component {
-  #eventSubscriptions;
   #components;
   #state;
   #pairState;
@@ -13,22 +12,15 @@ export class Component {
    * @param {{[string]: any}} initialState - {키: 밸류} 형식의 객체만 전달 가능합니다. 여기서의 키 값을 getState(), setState() 메서드에 전달해 상태를 조회, 수정할 수 있습니다.
    * @example App(document.querySelector('#app'), {tag: 'div', class: 'header'}, {count: 0})
    */
-  constructor($parent, { tag = "div", attrs = {} }, initialState) {
+  constructor(initialState) {
     if (new.target.prototype.constructor === Component) {
       throw new Error("추상 클래스를 인스턴스화 할 수 없습니다.");
     }
-
-    this.$target = document.createElement(tag);
-    $parent.appendChild(this.$target);
-    this.$parent = $parent;
-    this.attrs = attrs;
 
     this.#state = new BehaviorSubject(initialState ?? {});
     this.#pairState = this.#state.pipe(pairwise());
 
     this.addComponents();
-
-    this.render.call(this);
 
     this.#pairState
       .pipe(
@@ -36,15 +28,11 @@ export class Component {
         map(([, cur]) => cur)
       )
       .subscribe(() => {
-        this.render.call(this);
-        this.#components.forEach((comp) => {
-          comp.render();
-        });
+        console.log(this.$parent);
+        this.render(this.$parent);
       });
 
     // Mutation observer 공부하기
-    this.onUnmount = this.onMount();
-    this.#eventSubscriptions = this.addEvent();
 
     // const cb = (mutationList, observer) => {
     //   console.log(this.$parent.contains(this.$target));
@@ -60,14 +48,95 @@ export class Component {
     this.#components = Array.isArray(components) ? components : [components];
   }
 
-  render() {
+  render($parent) {
     //! App 컴포넌트가 없어짐. template에서 컴포넌트를 생성하도록, 또는 render 함수를 다시 작성
+    $parent.innerHTML = "";
+    this.$parent = $parent;
+    this.children = this.template(this.#state.getValue());
+    const rendered = this.children?.render($parent);
+    this.onUnmount = this.onMount();
+    return rendered;
+
     this.$parent.appendChild(this.$target);
     this.$target.innerHTML = this.template(this.#state.getValue());
     Object.keys(this.attrs).forEach((key) => {
       this.$target.setAttribute(key, this.attrs[key]);
     });
   }
+
+  // export class Component {
+  //   #eventSubscriptions;
+  //   #components;
+  //   #state;
+  //   #pairState;
+  //   /**
+  //    * @param {HTMLElement} $parent - 컴포넌트의 컨테이너
+  //    * @param {Object} target - 컨테이너 바로 아래 최상위 노드
+  //    * @param {string} target.tag - target의 태그명
+  //    * @param {string} target.attrs - target의 속성
+  //    * @param {{[string]: any}} initialState - {키: 밸류} 형식의 객체만 전달 가능합니다. 여기서의 키 값을 getState(), setState() 메서드에 전달해 상태를 조회, 수정할 수 있습니다.
+  //    * @example App(document.querySelector('#app'), {tag: 'div', class: 'header'}, {count: 0})
+  //    */
+  //   constructor($parent, { tag = "div", attrs = {} }, initialState) {
+  //     if (new.target.prototype.constructor === Component) {
+  //       throw new Error("추상 클래스를 인스턴스화 할 수 없습니다.");
+  //     }
+
+  //     this.$target = document.createElement(tag);
+  //     $parent.appendChild(this.$target);
+  //     this.$parent = $parent;
+  //     this.attrs = attrs;
+
+  //     this.#state = new BehaviorSubject(initialState ?? {});
+  //     this.#pairState = this.#state.pipe(pairwise());
+
+  //     this.addComponents();
+
+  //     this.render.call(this);
+
+  //     this.#pairState
+  //       .pipe(
+  //         filter(([prev, cur]) => prev != cur),
+  //         map(([, cur]) => cur)
+  //       )
+  //       .subscribe(() => {
+  //         this.render.call(this);
+  //         this.#components.forEach((comp) => {
+  //           comp.render();
+  //         });
+  //       });
+
+  //     // Mutation observer 공부하기
+  //     this.onUnmount = this.onMount();
+  //     this.#eventSubscriptions = this.addEvent();
+
+  //     // const cb = (mutationList, observer) => {
+  //     //   console.log(this.$parent.contains(this.$target));
+  //     // };
+  //     // const observer = new MutationObserver(cb);
+  //     // observer.observe(this.$parent, { childList: true });
+  //   }
+
+  //   /**
+  //    * @param  {Component[]} components
+  //    */
+  //   addComponents(components = []) {
+  //     this.#components = Array.isArray(components) ? components : [components];
+  //   }
+
+  //   render($parent) {
+  //     //! App 컴포넌트가 없어짐. template에서 컴포넌트를 생성하도록, 또는 render 함수를 다시 작성
+  //     this.$parent = $parent;
+  //     this.children = this.template();
+  //     const rendered = this.children.render($parent);
+  //     return rendered;
+
+  //     this.$parent.appendChild(this.$target);
+  //     this.$target.innerHTML = this.template(this.#state.getValue());
+  //     Object.keys(this.attrs).forEach((key) => {
+  //       this.$target.setAttribute(key, this.attrs[key]);
+  //     });
+  //   }
 
   /**
    * @param {any} state
@@ -108,6 +177,36 @@ export class MemoComponent extends Component {
     }
     super.render();
     this.memo = this.$target.innerHTML;
+  }
+}
+
+export class Element {
+  constructor(tag, attrs, children = []) {
+    this.tag = tag;
+    this.attrs = attrs || {};
+    this._children = Array.isArray(children) ? children : [children];
+  }
+
+  render($parent) {
+    this.element = document.createElement(this.tag);
+    Object.keys(this.attrs).forEach((key) => {
+      this.element.setAttribute(key, this.attrs[key]);
+    });
+
+    $parent.append(this.element);
+    this.children.forEach((child) => {
+      if (typeof child === "string" || typeof child === "number") {
+        this.element.innerText = child;
+      } else {
+        child.render(this.element);
+      }
+    });
+
+    return this.element;
+  }
+
+  get children() {
+    return this._children;
   }
 }
 
